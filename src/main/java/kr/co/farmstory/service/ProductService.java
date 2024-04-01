55f84e92eab0549230fb496762c3765d18afd5ea
kkj80911 <kkj80911@gmail.com>
2024-04-01 17:26:25 +0900

    product

---
+++ src/main/java/kr/co/farmstory/service/ProductService.java
@@ -0,0 +1,33 @@
+package kr.co.farmstory.service;
+
+import kr.co.farmstory.dto.ProductDTO;
+import kr.co.farmstory.entity.Product;
+import kr.co.farmstory.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j @Service @RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    private final ModelMapper modelMapper;
+
+    public ResponseEntity<List<ProductDTO>> selectProducts(){
+
+        log.info("selectProduct...1");
+        List<Product> productList = productRepository.findAll();
+
+        List<ProductDTO> productDTOS = productList.stream()
+                .map(entity-> modelMapper.map(entity, ProductDTO.class))
+                .toList();
+
+        return ResponseEntity.ok().body(productDTOS);
+    }
+
+}
