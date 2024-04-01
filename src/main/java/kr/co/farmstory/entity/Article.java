1d7cde673ef765f5ad463ff851d81df7b23941ef
JO9 <sardia0923@gmail.com>
2024-04-01 16:37:30 +0900

    article DTO, entity 작성

---
+++ src/main/java/kr/co/farmstory/entity/Article.java
@@ -0,0 +1,44 @@
+package kr.co.farmstory.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="article")
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int ano;
+
+    private String uid;
+    private int parent;
+
+    @Builder.Default
+    private int comment = 0;
+
+    private String cate;
+    private String title;
+    private String content;
+
+    @Builder.Default
+    private int hit = 0;
+
+    private String regip;
+
+    @CreationTimestamp
+    private LocalDateTime rdate;
+
+    @Builder.Default
+    private int file = 0;
+
+}
