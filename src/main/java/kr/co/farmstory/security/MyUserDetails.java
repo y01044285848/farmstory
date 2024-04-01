f503a0c0351495e6e0c447362dcd5a867a6b42e5
y01044285848 <y01044285848@gmail.com>
2024-04-01 10:52:50 +0900

    템플릿 업로드

---
+++ src/main/java/kr/co/farmstory/security/MyUserDetails.java
@@ -0,0 +1,63 @@
+package kr.co.farmstory.security;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@ToString
+@Builder
+public class MyUserDetails implements UserDetails {
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // 계정 만료 여부(true:만료안됨, false:만료)
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // 계정 잠김 여부(true:잠김안됨, false:잠김)
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // 비밀번호 만료 여부(true:만료안됨, false:만료)
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // 계정 활성화 여부(true:활성화, false:비활성화)
+        return true;
+    }
+
+
+}
---
+++ src/main/java/kr/co/farmstory/security/SecurityUserService.java
@@ -0,0 +1,22 @@
+package kr.co.farmstory.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class SecurityUserService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return null;
+    }
+}
