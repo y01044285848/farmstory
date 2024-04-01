01d89c9a6251da93711c69d3a270d089f083de45
dpsk3048 <dpsk3048@naver.com>
2024-04-02 03:02:57 +0900

    회원가입ing, 어드민 페이지 링크 연결

--- src/main/java/kr/co/farmstory/mapper/UserMapper.java
+++ src/main/java/kr/co/farmstory/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package kr.co.farmstory.mapper;
 
 import kr.co.farmstory.dto.TermsDTO;
+import kr.co.farmstory.dto.UserDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
@@ -8,4 +9,5 @@ public interface UserMapper {
 
     public TermsDTO selectTerms();
 
+    public void insertUser(UserDTO userDTO);
 }
--- src/main/java/kr/co/farmstory/service/UserService.java
+++ src/main/java/kr/co/farmstory/service/UserService.java
@@ -1,9 +1,11 @@
 package kr.co.farmstory.service;
 
 import kr.co.farmstory.dto.TermsDTO;
+import kr.co.farmstory.dto.UserDTO;
 import kr.co.farmstory.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -12,9 +14,17 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     public TermsDTO selectTerms(){
         return userMapper.selectTerms();
+    }
+
+    public void insertUser(UserDTO userDTO){
+        String encoded = passwordEncoder.encode(userDTO.getPass());
+        userDTO.setPass(encoded);
+
+        userMapper.insertUser(userDTO);
     }
 
 }
